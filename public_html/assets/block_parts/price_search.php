9231eb5e04bbdfdf6a5024b1c29c079206297704
kamenob <kameda@KzSystem.com>
2021-05-11 18:43:32 +0900

    update&fix:生前整理の料金ページ初稿

---
+++ public_html/assets/block_parts/price_search.php
@@ -0,0 +1,110 @@
+<section class="price__search-pc">
+<div class="price__search-pc_head">
+	<div>
+		<h2 class="l_section_title"><span class="small">片付け堂の</span><br>店舗別料金検索<span class="en">SEARCH</span></h2>
+	</div>
+	<p>
+		この文章はダミーです文字の大きさ・量・字間・行間等を確認するために入れています。この文章はダミーです。この文章はダミーです。この文章はダミーです文字の大きさ・量・字間・行間等を確認するために入れています。この文章はダミーです。
+	</p>
+</div>
+<div class="price__search-pc_map">
+	<figure><img src="../../assets/img/map.svg" alt="マップ"></figure>
+	<ul class="price__search-pc_map_links">
+		<li class="hokkaidou">
+			<span class="line01"></span>
+			<span class="line02"></span>
+			<div class="modal_open" data-modal="shops_hokkaidou">
+				<span>北海道地方の料金を見る</span>
+			</div>
+		</li>
+		<li class="kantou">
+			<span class="line01"></span>
+			<div class="modal_open" data-modal="shops_kantou">
+				<span>関東地方の料金を見る</span>
+			</div>
+		</li>
+		<li class="tyubu">
+			<span class="line01"></span>
+			<span class="line02"></span>
+			<span class="line03"></span>
+			<div class="modal_open" data-modal="shops_tyubu">
+				<span>中部地方の料金を見る</span>
+			</div>
+		</li>
+		<li class="kinki">
+			<span class="line01"></span>
+			<div class="modal_open" data-modal="shops_kinki">
+				<span>近畿地方の料金を見る</span>
+			</div>
+		</li>
+		<li class="tyugoku">
+			<span class="line01"></span>
+			<span class="line02"></span>
+			<span class="line03"></span>
+			<div class="modal_open" data-modal="shops_tyugoku">
+				<span>中国地方の料金を見る</span>
+			</div>
+		</li>
+		<li class="shikoku">
+			<span class="line01"></span>
+			<span class="line02"></span>
+			<div class="modal_open" data-modal="shops_shikoku">
+				<span>四国地方の料金を見る</span>
+			</div>
+		</li>
+		<li class="kyushu">
+			<span class="line01"></span>
+			<span class="line02"></span>
+			<div class="modal_open" data-modal="shops_kyushu">
+				<span>九州地方の料金を見る</span>
+			</div>
+		</li>
+	</ul>
+	<div class="price__search-pc_outline">
+		<p>
+			<strong>安心のお片付け専門</strong>の<span class="medium">全国チェーン！</span>
+			<span class="small">あなたの街の片付けパートナー</span>
+		</p>
+		<h3><img src="../../assets/img/logo.svg" alt="片付け堂"></h3>
+		<figure><img src="../../assets/img/shops_obj02.svg" alt="イメージ"></figure>
+	</div>
+</div>
+</section>
+<section class="price__search-sp">
+<h2 class="l_section_title"><span class="small">片付け堂の</span><br>店舗別料金検索<span class="en">SEARCH</span></h2>
+<div class="price__search-sp_outline">
+	<p>
+		<strong>安心のお片付け専門</strong>の<span class="medium">全国チェーン！</span>
+		<span class="small">あなたの街の片付けパートナー</span>
+	</p>
+	<h3><img src="../../assets/img/logo.svg" alt="片付け堂"></h3>
+	<figure class="map"><img src="../../assets/img/sp/map.svg" alt="イメージ"></figure>
+	<figure class="obj"><img src="../../assets/img/shops_obj02.svg" alt="イメージ"></figure>
+</div>
+<p>
+	この文章はダミーです文字の大きさ・量・字間・行間等を確認するために入れています。この文章はダミーです。この文章はダミーです。この文章はダミーです文字の大きさ・量・字間・行間等を確認するために入れています。この文章はダミーです。
+</p>
+<ul class="price__search-sp_map_links">
+	<li class="modal_open" data-modal="shops_hokkaidou">
+		<span>北海道地方の料金を見る</span>
+	</li>
+	<li class="modal_open" data-modal="shops_kantou">
+		<span>関東地方の料金を見る</span>
+	</li>
+	<li class="modal_open" data-modal="shops_tyubu">
+		<span>中部地方の料金を見る</span>
+	</li>
+	<li class="modal_open" data-modal="shops_kinki">
+		<span>近畿地方の料金を見る</span>
+	</li>
+	<li class="modal_open" data-modal="shops_tyugoku">
+		<span>中国地方の料金を見る</span>
+	</li>
+	<li class="modal_open" data-modal="shops_shikoku">
+		<span>四国地方の料金を見る</span>
+	</li>
+	<li class="modal_open" data-modal="shops_kyushu">
+		<span>九州地方の料金を見る</span>
+	</li>
+</ul>
+</section>
